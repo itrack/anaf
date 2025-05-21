019a669c920b3719f376b5cd55882c3a3ff897a1
marius <marius@seomonitor.com>
2025-05-21 15:00:21 +0300

    Update test cases for address validation and adjust CIF limit to 100

--- tests/Unit/HttpTest.php
+++ tests/Unit/HttpTest.php
@@ -9,8 +9,8 @@ class HttpTest extends TestCase
     public function testCifLimitException()
     {
         $this->expectException(Exceptions\LimitExceeded::class);
-        $this->expectExceptionMessage("You can check one time up to 500 cifs.");
+        $this->expectExceptionMessage("You can check one time up to 100 cifs.");
         
-        Http::call(array_fill(0, 501, '123456'));
+        Http::call(array_fill(0, 101, '123456'));
     }
 }
